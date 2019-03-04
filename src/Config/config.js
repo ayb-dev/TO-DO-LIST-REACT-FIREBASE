d3fbc2e4b6bd30cd3439b8f99a40b0b70d5b8638
ayoub <fadadi.ayoub@gmail.com>
2019-03-04 13:11:50 +0100

    upload changes to todolist

---
+++ src/Config/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export const DB_CONFIG = {
+    apiKey: "AIzaSyD23sLBj03EMuHgF3lGUFNifxywVcgO61Y",
+    authDomain: "react-notes-7d494.firebaseapp.com",
+    databaseURL: "https://react-notes-7d494.firebaseio.com",
+    projectId: "react-notes-7d494",
+    storageBucket: "react-notes-7d494.appspot.com",
+    messagingSenderId: "1082407328667"
+};
